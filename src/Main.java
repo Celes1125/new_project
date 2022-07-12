f024e5bedaecb36b763fb7ac8261d506afc35abe
cc <colauttiadad@gmail.com>
2022-07-12 18:03:10 -0300

    Menu principal y métodos instanciados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    static java.util.HashSet<User> users;
+    static java.util.Scanner scanner;
+
+    public Main() { /* compiled code */ }
+
+    public static void main(java.lang.String[] args) { /* compiled code */ }
+
+    public static void createUser() { /* compiled code */ }
+
+    public static void setUser() { /* compiled code */ }
+
+    public static void deleteUser() { /* compiled code */ }
+
+    public static void createCategory() { /* compiled code */ }
+
+    public static void setCategory() { /* compiled code */ }
+
+    public static void deleteCategory() { /* compiled code */ }
+
+    public static void createSubcategory() { /* compiled code */ }
+
+    public static void setSubcategory() { /* compiled code */ }
+
+    public static void deleteSubcategory() { /* compiled code */ }
+
+    public static void recordOutlay() { /* compiled code */ }
+
+    public static void recordIncome() { /* compiled code */ }
+
+    public static void checkBalance() { /* compiled code */ }
+
+    public static void listUsers() { /* compiled code */ }
+
+    public static void listCategories() { /* compiled code */ }
+
+    public static void listSubcategories() { /* compiled code */ }
 }
---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+    public java.lang.String name;
+    public java.lang.String email;
+
+    public User(java.lang.String name, java.lang.String email) { /* compiled code */ }
+
+    public java.lang.String getName() { /* compiled code */ }
+
+    public void setName(java.lang.String name) { /* compiled code */ }
+
+    public java.lang.String getEmail() { /* compiled code */ }
+
+    public void setEmail(java.lang.String email) { /* compiled code */ }
+}
