d4b611a0f44391cb51235e40a56acb5c3747885b
cc <colauttiadad@gmail.com>
2022-07-14 20:26:45 -0300

    Menu principal corregido

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,60 @@ import java.util.Scanner;
 public class Main {
     static HashSet<User> users = new HashSet<User>();
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        int option=0;
+        //MENU PRINCIPAL
+
+        int option = 0;
 
         do {
-            System.out.println("OPCIONES");
+            System.out.println("Seleccione la opción deseada");
+            System.out.println("1. USUARIO");
+            System.out.println("2. CATEGORIA");
+            System.out.println("3. SUBCATEGORÍA");
+            System.out.println("4. OPERACIONES");
+            System.out.println("5. SALIR");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    showOptionsForUser();
+                    break;
+                case 2:
+                    showOptionsForCategory();
+                    break;
+                case 3:
+                    showOptionsforSubcategory();
+                    break;
+                case 4:
+                    showOptionsforOperations();
+                    break;
+                case 5:
+                    System.out.println("Gracias por utilizar nuestros servicios");
+                    break;
+                default:
+                    System.out.println("Por favor, seleccione una opción válida");
+
+            }
+        } while (option != 5);
+
+
+    }
+
+
+    //MÉTODOS PARA NAVEGAR EL MENÚ
+
+    public static void showOptionsForUser() {
+        int option = 0;
+        do {
+            System.out.println("Seleccione la opción deseada");
             System.out.println("1. CREAR USUARIO");
             System.out.println("2. MODIFICAR USUARIO");
             System.out.println("3. ELIMINAR USUARIO");
-            System.out.println("4. CREAR CATEGORÍA");
-            System.out.println("5. MODIFICAR CATEGORÍA");
-            System.out.println("6. ELIMINAR CATEGORÍA");
-            System.out.println("7. CREAR SUBCATEGORÍA");
-            System.out.println("8. MODIFICAR SUBCATEGORÍA");
-            System.out.println("9. ELIMINAR SUBCATEGORÍA");
-            System.out.println("10. REGISTRAR GASTO");
-            System.out.println("11. REGISTRAR INGRESO");
-            System.out.println("12. MOSTRAR SALDO");
-            System.out.println("13. LISTAR USUARIOS");
-            System.out.println("14. LISTAR CATEGORÍAS");
-            System.out.println("15. LISTAR SUBCATEGORÍAS");
-
+            System.out.println("4. LISTAR USUARIOS");
+            System.out.println("5. VOLVER AL MENU PRINCIPAL");
 
             option = scanner.nextInt();
 
@@ -41,49 +73,123 @@ public class Main {
                     deleteUser();
                     break;
                 case 4:
-                    createCategory();
-                    break;
-                case 5:
-                    setCategory();
-                    break;
-                case 6:
-                    deleteCategory();
-                    break;
-                case 7:
-                    createSubcategory();
-                    break;
-                case 8:
-                    setSubcategory();
-                    break;
-                case 9:
-                    deleteSubcategory();
-                    break;
-                case 10:
-                    recordOutlay();
-                    break;
-                case 11:
-                    recordIncome();
-                    break;
-                case 12:
-                    checkBalance();
-                    break;
-                case 13:
                     listUsers();
                     break;
-                case 14:
-                    listCategories();
-                    break;
-                case 15:
-                    listSubcategories();
+                case 5:
+                    System.out.println("Adios!");
                     break;
                 default:
                     System.out.println("Por favor, seleccione una opción válida");
-
             }
-        }while (option != 16);
-        System.out.println("Gracias por utilizar nuestros servicios jajaaj");
+        } while (option != 5);
 
     }
+
+    public static void showOptionsForCategory() {
+        int option = 0;
+        do {
+
+            System.out.println("Seleccione la opción deseada");
+            System.out.println("1. CREAR CATEGORÍA");
+            System.out.println("2. MODIFICAR CATEGORÍA");
+            System.out.println("3. ELIMINAR CATEGORÍA");
+            System.out.println("4. LISTAR CATEGORÍAS");
+            System.out.println("5. VOLVER AL MENU PRINCIPAL");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    createCategory();
+                    break;
+                case 2:
+                    setCategory();
+                    break;
+                case 3:
+                    deleteCategory();
+                    break;
+                case 4:
+                    listCategories();
+                    break;
+                case 5:
+                    System.out.println("Adios!");
+                    break;
+                default:
+                    System.out.println("Por favor, seleccione una opción válida");
+            }
+        } while (option != 5);
+
+    }
+
+    public static void showOptionsforSubcategory() {
+        int option = 0;
+        do {
+
+            System.out.println("Seleccione la opción deseada");
+            System.out.println("1. CREAR SUBCATEGORÍA");
+            System.out.println("2. MODIFICAR SUBCATEGORÍA");
+            System.out.println("3. ELIMINAR SUBCATEGORÍA");
+            System.out.println("4. LISTAR SUBCATEGORÍAS");
+            System.out.println("5. VOLVER AL MENU PRINCIPAL");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    createSubcategory();
+                    break;
+                case 2:
+                    setSubcategory();
+                    break;
+                case 3:
+                    deleteSubcategory();
+                    break;
+                case 4:
+                    listSubcategories();
+                    break;
+                case 5:
+                    System.out.println("Adios!");
+                    break;
+                default:
+                    System.out.println("Por favor, seleccione una opción válida");
+            }
+        } while (option != 5);
+
+
+    }
+
+    public static void showOptionsforOperations() {
+        int option = 0;
+        do {
+
+            System.out.println("Seleccione la opción deseada");
+            System.out.println("1. REGISTRAR GASTO");
+            System.out.println("2. REGISTRAR INGRESO");
+            System.out.println("3. MOSTRAR SALDO");
+            System.out.println("4. VOLVER AL MENU PRINCIPAL");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    recordOutlay();
+                    break;
+                case 2:
+                    recordIncome();
+                    break;
+                case 3:
+                    checkBalance();
+                    break;
+                case 4:
+                    System.out.println("Adios!");
+                    break;
+                default:
+                    System.out.println("Por favor, seleccione una opción válida");
+            }
+        } while (option != 4);
+    }
+
+    //MÉTODOS PARA CADA OPERACIÓN
 
     public static void createUser() {
         System.out.println("Proporcione el nombre del nuevo usuario");
@@ -103,10 +209,10 @@ public class Main {
         String name = scanner.next();
         System.out.println("Proporcione el nuevo nombre de usuario");
         String newName = scanner.next();
-        for (User user: users ) {
-            if(user.getName() == name) {
+        for (User user : users) {
+            if (user.getName() == name) {
                 user.setName(newName);
-            }else{
+            } else {
                 System.out.println("El usuario " + name + " no existe. Por favor ingrese un nombre de usuario válido");
             }
         }
@@ -164,15 +270,17 @@ public class Main {
 
     }
 
-    public static void listUsers (){
-        for (User user: users) {
-            System.out.println("Nombre: "+ user.getName() + " E-mail: " + user.getEmail());
+    public static void listUsers() {
+        for (User user : users) {
+            System.out.println("Nombre: " + user.getName() + " E-mail: " + user.getEmail());
         }
     }
-    public static void listCategories (){
+
+    public static void listCategories() {
         System.out.println("Hi!");
     }
-    public static void listSubcategories (){
+
+    public static void listSubcategories() {
         System.out.println("Hi!");
     }
 
